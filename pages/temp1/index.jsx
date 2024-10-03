30d5abee1a580f885bbff9750ca00e4b2b022077
terry <terryip808@gmail.com>
2024-10-03 17:18:10 +0800

    update

--- pages/temp1/index.jsx
+++ pages/temp1/index.jsx
@@ -3,10 +3,7 @@ import React from 'react';
 import Nav from '../components/Nav';
 import Sidebar from '../components/Sidebar';
 import Footer from '../components/Footer';
-import SlideImg from '../components/SlideImg';
-import Responsive from "../components/responsive";
 import GoogleMapComponent from "../components/GoogleMapComponent";
-import { Height } from "@mui/icons-material";
 
 function Home() {
   
@@ -22,8 +19,10 @@ function Home() {
       <Nav />
       <Sidebar />
       {/* <Responsive style={{ flex:1}}/> */}
-      <div>
-      <GoogleMapComponent style={{ flex:1}} />
+      
+      <div className="flex p-10 bg-emerald-200 md:w-[1600px]">
+      <GoogleMapComponent />
+      <p className="ml-10 text-3xl text-white">地址:21654</p>
       </div>
       
       <Footer />
