2a0f104cf95ddb64db0c824b9150a13002303455
Thirteenneway <samliu13@hotmail.com>
2024-10-08 15:44:44 +0800

    change the background color and the buttons place.

--- pages/components/VideoPlayer.jsx
+++ pages/components/VideoPlayer.jsx
@@ -45,27 +45,28 @@ const VideoPlayer = ({ videoIds }) => {
     return (
         <div className="video-container">
             <div className="video-wrapper">
-            {videoIds.map((videoId, index) => (
-                <iframe
-                    key={videoId}
-                    ref={(el) => (iframeRefs.current[index] = el)}
-                    className={index === currentVideoIndex ? 'active' : 'hidden'}
-                    width="100%"
-                    height="100%"
-                    src={`https://www.youtube.com/embed/${videoId}?autoplay=${index === currentVideoIndex ? 1 : 0}&enablejsapi=1`}
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                ></iframe>
-            ))}
+                {videoIds.map((videoId, index) => (
+                    <iframe
+                        key={videoId}
+                        ref={(el) => (iframeRefs.current[index] = el)}
+                        className={index === currentVideoIndex ? 'active' : 'hidden'}
+                        width="100%"
+                        height="100%"
+                        src={`https://www.youtube.com/embed/${videoId}?autoplay=${index === currentVideoIndex ? 1 : 0}&enablejsapi=1`}
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                    ></iframe>
+                ))}
+            </div>
             <div className="button-container">
-            <button className='nav-btn prev-btn' id="prev-btn" onClick={handlePrevVideo}>
-                <ArrowBackIcon /> 
-            </button>
-            <button className='nav-btn next-btn' id="next-btn" onClick={handleNextVideo}>
-                <ArrowForwardIcon />            
-            </button>
-        </div>
+                <button className='nav-btn prev-btn' id="prev-btn" onClick={handlePrevVideo}>
+                    <ArrowBackIcon />
+                </button>
+                <button className='nav-btn next-btn' id="next-btn" onClick={handleNextVideo}>
+                    <ArrowForwardIcon />
+                </button>
+
             </div>
         </div>
     );
