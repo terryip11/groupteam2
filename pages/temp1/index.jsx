11615d523a4041aed3ba007aa349960626c45004
terry <terryip808@gmail.com>
2024-10-08 12:45:02 +0800

    update

--- pages/temp1/index.jsx
+++ pages/temp1/index.jsx
@@ -13,13 +13,7 @@ function Home() {
 
       <Nav />
       <Sidebar />
-      {/* <Responsive style={{ flex:1}}/> */}
-      
-      <div className="flex p-10 bg-emerald-200 md:w-[1600px]">
       <GoogleMapComponent />
-      <p className="ml-10 text-3xl text-white">地址:21654</p>
-      </div>
-      
       <Footer />
     </div>
   );
