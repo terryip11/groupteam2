b6cc38049828da730a5273d5ccd1c75f26a71adf
Thirteenneway <samliu13@hotmail.com>
2024-10-09 09:34:08 +0800

    sam

--- pages/temp1/index.jsx
+++ pages/temp1/index.jsx
@@ -6,18 +6,17 @@ import Footer from '../components/Footer';
 import ClubDescription from '../components/ClubDescription';
 import GoogleMapComponent from "../components/GoogleMapComponent";
 
-function Home() {
+function Temp1() {
   
 
   return (
-<<<<<<< HEAD
     <>
         <Nav />
         <Sidebar />
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
             <img src="images/TheWanch.jpg" alt="TheWanch" width={800} />
         </div>
-        {/* <ClubDescription clubId={1} /> */}
+        <ClubDescription clubId={1} />
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
             
         </div>
@@ -27,16 +26,6 @@ function Home() {
         <Footer />
     </>
     );
-=======
-    <div>
-
-      <Nav />
-      <Sidebar />
-      <GoogleMapComponent />
-      <Footer />
-    </div>
-  );
->>>>>>> 5b24207132fa63d41ca0d117d1424da73abf0e06
 }
 
 export default Temp1;
