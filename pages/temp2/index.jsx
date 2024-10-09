832a183b4d0196db8a3a5d3a9494635db6abdb16
Thirteenneway <samliu13@hotmail.com>
2024-10-09 11:15:59 +0800

    sam

--- pages/temp2/index.jsx
+++ pages/temp2/index.jsx
@@ -4,6 +4,7 @@ import Nav from '../components/Nav';
 import Sidebar from '../components/Sidebar';
 import Footer from '../components/Footer';
 import ClubDescription from '../components/ClubDescription';
+import VideoTest from "../test/VideoTest";
 
 function Temp2() {
     return (
@@ -14,18 +15,7 @@ function Temp2() {
                 <img src="images/TheWanch.jpg" alt="TheWanch" width={800} />
             </div>
             <ClubDescription clubId={1} />
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
-                <iframe
-                    width="560"
-                    height="315"
-                    src="https://www.youtube.com/embed/sXSNymE8Wio?si=e46-NiP4W93djWjq"
-                    title="YouTube video player"
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    referrerPolicy="strict-origin-when-cross-origin"
-                    allowFullScreen
-                />
-            </div>
+            <VideoTest />
             <Footer />
         </>
     );
---
+++ pages/test/VideoTest.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useRef } from 'react';
+
+
+const VideoTest = () => {
+    return (
+        <div> 
+            <iframe 
+            // width="100%"
+            height="100%" 
+            src="https://www.youtube.com/embed/AjVCx4nN2dg?si=ROIZlcO9QWFKz2Hd" 
+            frameborder='0' 
+            allowfullscreen >
+            </iframe>"
+        </div>
+    );
+};
+export default  VideoTest; 
