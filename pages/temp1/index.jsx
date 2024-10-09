da60936e7d609d63896508e7c7ddea11455c4d87
Fung <iamtimtam88@gmail.com>
2024-10-09 09:39:39 +0800

    abc

--- pages/temp1/index.jsx
+++ pages/temp1/index.jsx
@@ -3,31 +3,12 @@ import React from 'react';
 import Nav from '../components/Nav';
 import Sidebar from '../components/Sidebar';
 import Footer from '../components/Footer';
-import ClubDescription from '../components/ClubDescription';
 import GoogleMapComponent from "../components/GoogleMapComponent";
 
-function Home() {
+function Temp1() {
   
 
   return (
-<<<<<<< HEAD
-    <>
-        <Nav />
-        <Sidebar />
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
-            <img src="images/TheWanch.jpg" alt="TheWanch" width={800} />
-        </div>
-        {/* <ClubDescription clubId={1} /> */}
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
-            
-        </div>
-        <div>
-        <GoogleMapComponent />
-        </div>
-        <Footer />
-    </>
-    );
-=======
     <div>
 
       <Nav />
@@ -36,7 +17,6 @@ function Home() {
       <Footer />
     </div>
   );
->>>>>>> 5b24207132fa63d41ca0d117d1424da73abf0e06
 }
 
 export default Temp1;
