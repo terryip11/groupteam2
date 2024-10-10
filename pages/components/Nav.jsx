2bc1b79950bb78d1464db09c48821894750c5a4a
Fung <iamtimtam88@gmail.com>
2024-10-10 16:50:34 +0800

    Added 12 bar pages and updated Nav bar link to 12 bar

--- pages/components/Nav.jsx
+++ pages/components/Nav.jsx
@@ -3,6 +3,18 @@ import { useState } from "react";
 import logo from '../../public/images/rock-and-roll-music-Poster.jpg';import Image from "next/image";
 import Link from "next/link";
 import PopupLogin from "./PopupLogin";
+import "../Club/Wanch"
+import "../Club/Rev_Now"
+import "../Club/The_Rock_Pub"
+import "../Club/Rock_Factory"
+import "../Club/Bauhaus_Roppongi"
+import "../Club/Rockaholic_Shibuya_Music_Bar"
+import "../Club/12_Monkeys_Music_Hall_&_Pub"
+import "../Club/Club_FF"
+import "../Club/Hanoi_Rock_City"
+import "../Club/The_Flying_V_Metal_Bar"
+import "../Club/Tom_Dick_&_Harry"
+import "../Club/Gimme_Shelter_Bali"
 
 
 function Nav() {
@@ -39,18 +51,18 @@ function Nav() {
 
             <li className="group flex flex-col relative mt-8 md:mt-0 text-center hover:text-blue-500 rounded-lg">Venue
               <div className="hidden absolute bg-gray-500 z-50 group-hover:block top-9 w-96 rounded-lg shadow-md shadow-gray-500 text-white textstyle" >
-                <Link href="https://www.thewanch.hk" target="_blank" className="block mt-2 hover:bg-green-500">The Wanch</Link>
-                <Link href="https://www.facebook.com/revnowlive" className="block mt-2 hover:bg-green-500">Rev-Now</Link>
-                <Link href="https://www.facebook.com/therockpub/" className="block mt-2 hover:bg-green-500">The Rock Pub</Link>
-                <Link href="https://www.facebook.com/Rockfactorypattaya/" className="block mt-2 hover:bg-green-500">Rock Factory</Link>
-                <Link href="https://rockbarbauhaus.com" className="block mt-2 hover:bg-green-500">Bauhaus Roppongi</Link>
-                <Link href="https://bar-rockaholic.jp/shibuya/" className="block mt-2 hover:bg-green-500">Rockaholic Shibuya Music Bar</Link>
-                <Link href="https://www.facebook.com/12MonkeysPH/" className="block mt-2 hover:bg-green-500">Monkeys Music Hall & Pub</Link>
-                <Link href="https://clubff.modoo.at" className="block mt-2 hover:bg-green-500">Club FF</Link>
-                <Link href="https://hrcwelive.com/en/homepage-en/" className="block mt-2 hover:bg-green-500">Hanoi Rock City</Link>
-                <Link href="https://www.theflyingvmetalbar.com" className="block mt-2 hover:bg-green-500">The Flying V Metal Bar</Link>
-                <Link href="https://www.facebook.com/TDHmalaysia/" className="block mt-2 hover:bg-green-500">Tom, Dick & Harry's</Link>
-                <Link href="https://www.facebook.com/gimmeshelterbali/about" className="block mt-2 hover:bg-green-500">Gimme Shelter Bali</Link>
+                <Link href="../Club/Wanch" target="_blank" className="block mt-2 hover:bg-green-500">The Wanch</Link>
+                <Link href="../Club/Rev_Now" target="_blank" className="block mt-2 hover:bg-green-500">Rev-Now</Link>
+                <Link href="../Club/The_Rock_Pub/" target="_blank" className="block mt-2 hover:bg-green-500">The Rock Pub</Link>
+                <Link href="../Club/Rock_Factory/" target="_blank" className="block mt-2 hover:bg-green-500">Rock Factory</Link>
+                <Link href="../Club/Bauhaus_Roppongi" target="_blank" className="block mt-2 hover:bg-green-500">Bauhaus Roppongi</Link>
+                <Link href="../Club/Rockaholic_Shibuya_Music_Bar/" target="_blank" className="block mt-2 hover:bg-green-500">Rockaholic Shibuya Music Bar</Link>
+                <Link href="../Club/12_Monkeys_Music_Hall_&_Pub/" target="_blank" className="block mt-2 hover:bg-green-500">Monkeys Music Hall & Pub</Link>
+                <Link href="../Club/Club_FF" target="_blank"className="block mt-2 hover:bg-green-500">Club FF</Link>
+                <Link href="../Club/Hanoi_Rock_City/" target="_blank" className="block mt-2 hover:bg-green-500">Hanoi Rock City</Link>
+                <Link href="../Club/The_Flying_V_Metal_Bar" target="_blank" className="block mt-2 hover:bg-green-500">The Flying V Metal Bar</Link>
+                <Link href="../Club/Tom_Dick_&_Harry/" target="_blank" className="block mt-2 hover:bg-green-500">Tom, Dick & Harry's</Link>
+                <Link href="../Club/Gimme_Shelter_Bali" target="_blank" className="block mt-2 hover:bg-green-500">Gimme Shelter Bali</Link>
               </div>
             </li>
 
