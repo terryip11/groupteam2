c819bb41d2f55a038213a73d8aeca99876593eff
terry <terryip808@gmail.com>
2024-10-11 16:52:15 +0800

    update

--- pages/Club/The_Rock_Pub/ClubImage_Video.jsx
+++ pages/Club/The_Rock_Pub/ClubImage_Video.jsx
@@ -1,11 +1,7 @@
 'use client'
 import React from 'react';
 import Image from 'next/image';
-<<<<<<< HEAD
 import TheRockPub from '../../../public/images/TheRockPub.jpg';
-=======
-// import Monkeys from '../../../public/images/The Rock Pub.jpg';
->>>>>>> 6c40e682cb4fecb21f43d9980f1b4edf5c7fd69f
 import ClubDescription from '../../components/ClubDescription';
 
 const ClubImage_Video = () => {
@@ -14,11 +10,7 @@ const ClubImage_Video = () => {
       <div className="text-5xl text-center bg-gray-400 text-white hover:text-blue-600 textstyle">Rock Pub</div>
         <div className="flex justify-center">
           <Image
-<<<<<<< HEAD
             src={TheRockPub.src}
-=======
-            src={'./images/The Rock Pub.jpg'}
->>>>>>> 6c40e682cb4fecb21f43d9980f1b4edf5c7fd69f
             alt="Mountains"
             width={1000}
             height={1000}
