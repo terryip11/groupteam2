101a1cdc02fe857dd6df46eace8fd293c8c36445
Thirteenneway <samliu13@hotmail.com>
2024-10-15 16:07:42 +0800

    sam update

---
+++ pages/contact/index.jsx
@@ -0,0 +1,38 @@
+// pages/contact.js
+
+import Head from 'next/head';
+import "../../app/globals.css";
+import React from 'react';
+import Nav from "../components/Nav";
+import Sidebar from "../components/Sidebar";
+import Footer from "../components/Footer";
+import ContactForm from "../components/ContactForm";
+
+export default function Contact() {
+  return (
+    <div>
+      <Head>
+        <title>Contact Us</title>
+        <meta name="description" content="Contact page for our company" />
+      </Head>
+
+      <Nav />
+      <Sidebar />
+      <div style={{ margin: '15px' , fontSize: '30px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>        
+        <h1>Contact Us</h1>
+      </div>
+      <div className="contactInfo">
+        <p>Address: 81 Austin Road, Tsim Sha Tsui</p>
+        <p>E-mail: aeltd@gmail.com</p>
+        <p>Phone: 852 2730 5653</p>
+        <p>Fax: 852 2736 0612</p>
+
+      </div>
+      <div className="contactForm">
+        <ContactForm />
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
