adc96329462b179965dafb99022f19aefacf68d3
Thirteenneway <samliu13@hotmail.com>
2024-10-17 11:17:52 +0800

    sam update booking link name

---
+++ pages/Booking2/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Booking from '../booking/components/Booking';
+import Login from '../booking/components/login';
+
+export default function index() {
+  return (
+    <>
+        
+        <Booking />
+        
+    </>
+  )
+}
